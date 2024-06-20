15a179434bbca92e807405555cede8c0869d6e6f
OM <om@OMs-MacBook-Air.attlocal.net>
2024-06-19 21:03:25 -0700

    Adding create user

--- app.js
+++ app.js
@@ -1,45 +1,48 @@
-const http = require('http');
-const { queryDatabase } = require('./db.js');
-const sql = require('mssql');
-const {getUsers, getClasses, getAssignments} = require('./process.js')
+const express = require('express');
+const app = express();
+const {getUsers, getClasses, getAssignments, createClass} = require('./process.js')
 
-
-const server = http.createServer( async(req, res) => {
-if (req.url === '/')    {
-    res.write('Hello World');
-    res.end();
-}
-else if(req.url.startsWith('/api/getuser')) {
-    const results = await getUsers(req);
+app.get('/api/getuser', async (req, res) => {
+    const queryParams = req.query;
+    const results = await getUsers(queryParams.email_id);
     res.write(JSON.stringify(results));
     res.end();
-}
-else if(req.url.startsWith('/api/verifyuser')) {
-    res.write('verifyUser');
-    res.end();
-}
-else if(req.url.startsWith('/api/createclass')) {
-    res.write('create class');
-    res.end();
-}
-else if(req.url.startsWith('/api/obtainclasses')) {
-    const results = await getClasses(req);
-    res.write(JSON.stringify(results));
-    res.end();
-}
-else if(req.url.startsWith('/api/createassignment')) {
-    res.write('createassignment');
-    res.end();
-}
-else if(req.url.startsWith('/api/obtainassignments')) {
-    const results = await getAssignments(req);
-    res.write(JSON.stringify(results));
-    res.end();
-}
-else {
-    res.write("Endpoint Not Found");
-    res.end();
-}
 });
-server.listen(9999);
-console.log('listening on port 9999');
+
+app.get('/api/verifyuser', async (req, res) => {
+     res.write("verifyuser called");
+    res.end();
+});
+
+app.get('/api/obtainclasses', async (req, res) => {
+    const queryParams = req.query;
+    const results = await getClasses(queryParams.email_id);
+    res.write(JSON.stringify(results));
+    res.end();
+});
+
+app.get('/api/obtainassignments', async (req, res) => {
+    const queryParams = req.query;
+    const results = await getAssignments(queryParams.email_id);
+    res.write(JSON.stringify(results));
+    res.end();
+});
+
+app.post('/api/createclass', express.json(), async (req, res) => {
+    const queryParams = req.query;
+    const body = req.body;
+    await createClass(queryParams.email_id, body);
+    res.json({ body });
+  });
+
+app.post('/api/createassignment', express.json(), async (req, res) => {
+    // Access body data
+    const body = req.body;
+    console.log('Body Data:', body);
+    res.json({ body });
+  });
+
+const PORT = 9999;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
--- process.js
+++ process.js
@@ -3,39 +3,78 @@ const http = require('http');
 const { queryDatabase } = require('./db.js');
 
 
-  const getUsers = async (req) => {
-    const parsedUrl = new URL(req.url, `https://${req.headers.host}/`);
-    const emailID = parsedUrl.searchParams.get('email_id');
+  const getUsers = async (email_id) => {
     const query = 'SELECT * FROM tbl_users WHERE email_id = @emailID';
     const params = [
-        { name: 'emailID', type: sql.VarChar, value: emailID }
+        { name: 'emailID', type: sql.VarChar, value: email_id }
     ];
     const results = await queryDatabase(query, params);
     return results;
 }
 
-const getClasses = async (req) => {
-    const parsedUrl = new URL(req.url, `https://${req.headers.host}/`);
-    const emailID = parsedUrl.searchParams.get('email_id');
+const getClasses = async (email_id) => {
     const query = 'SELECT * FROM tbl_classes WHERE teacher_email_id = @emailID';
     const params = [
-        { name: 'emailID', type: sql.VarChar, value: emailID }
+        { name: 'emailID', type: sql.VarChar, value: email_id }
     ];
     const results = await queryDatabase(query, params);
     return results;
 }
 
-const getAssignments = async (req) => {
-    const parsedUrl = new URL(req.url, `https://${req.headers.host}/`);
-    const emailID = parsedUrl.searchParams.get('email_id');
-    const query = 'SELECT * FROM tbl_assignments WHERE owner_email_id = @emailID';
+const getAssignments = async (email_id) => {
+    const query = 'SELECT * FROM tbl_assignments WHERE id in (Select assignment_id from tbl_user_assignment_mappings WHERE user_email_id = @emailID)';
     const params = [
-        { name: 'emailID', type: sql.VarChar, value: emailID }
+        { name: 'emailID', type: sql.VarChar, value: email_id }
     ];
     const results = await queryDatabase(query, params);
     return results;
+}
+
+const createClass = async (email_id, class_details) => {
+    let users = await getUsers(email_id);
+    if(users && users.length == 1) {
+        user_email_id = users[0].email_id;
+        user_type = users[0].user_type;
+        teacher_id = users[0].id;
+        class_name = class_details.class_name;
+        email_ids = class_details.students_email_ids;
+
+        const query = 'INSERT INTO tbl_classes (teacher_id,class_name,teacher_email_id,email_ids)'+
+        'VALUES (@teacherID, @className, @teacherEmailID, @emailIDs); SELECT SCOPE_IDENTITY() AS InsertedID;';
+        const params = [
+            { name: 'teacherID', type: sql.Int, value: teacher_id },
+            { name: 'className', type: sql.VarChar, value: class_name },
+            { name: 'teacherEmailID', type: sql.VarChar, value: user_email_id },
+            { name: 'emailIDs', type: sql.VarChar, value: email_ids }
+        ];
+        const result =  await queryDatabase(query, params);
+
+        const class_id = result[0].InsertedID;
+        if(user_type === 'TEACHER') {
+            const enrolled_user_email_ids = email_ids.split(',');
+            enrolled_user_email_ids.push(user_email_id);
+            const uniqueEmailIds = [...new Set(enrolled_user_email_ids)];
+            uniqueEmailIds.forEach( async (email_id, index) => {
+                const query = 'INSERT INTO tbl_user_class_mappings (class_id,user_email_id)'+
+                'VALUES (@classID, @userEmailID)';
+                const params = [
+                    { name: 'classID', type: sql.Int, value: class_id },
+                    { name: 'userEmailID', type: sql.VarChar, value: email_id }
+                ];
+                await queryDatabase(query, params);
+                });
+        } else {
+            const query = 'INSERT INTO tbl_user_class_mappings (class_id,user_email_id)'+
+                'VALUES (@classID, @userEmailID)';
+            const params = [
+                { name: 'classID', type: sql.Int, value: class_id },
+                { name: 'userEmailID', type: sql.VarChar, value: user_email_id }
+            ];
+            await queryDatabase(query, params);
+        }
+    }
 }
 
 module.exports = {
-    getUsers, getClasses, getAssignments
+    getUsers, getClasses, getAssignments, createClass
 };
